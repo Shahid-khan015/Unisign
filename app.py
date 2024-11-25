60ef7cf1d0f88f3e4a5f8f9530e6e6ecb9f2419f
Shahid-khan015 <khan.shahidljpit2020@gmail.com>
2024-11-25 13:50:30 +0530

    Added Commits

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from website import create_app
 
-app  = create_app()
-# app.secret_key = '12@#!_#SD'
-
+app = create_app()
 application = app
 
+# Remove debug mode for production
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
